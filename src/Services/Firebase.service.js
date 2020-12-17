8a543c4b45c3ed2076a8d35801560c483356f9fd
Steven Carter <steven.carter19891025@gmail.com>
2020-12-18 02:31:15 +0530

    Initialize firebase

---
+++ src/Services/Firebase.service.js
@@ -0,0 +1,12 @@
+import firebase from "firebase"
+
+const firebase = firebase.initializeApp({
+    apiKey: "AIzaSyDX_cXUFGR9TV7vnr59VwFwXb_X2C3MKtc",
+    authDomain: "pocket-cast-c272a.firebaseapp.com",
+    databaseURL: "https://pocket-cast-c272a.firebaseio.com",
+    projectId: "pocket-cast-c272a",
+    storageBucket: "pocket-cast-c272a.appspot.com",
+    messagingSenderId: "413944153011",
+    appId: "1:413944153011:web:a663c2ceb63579393901c6",
+    measurementId: "G-DXTR94Q382"
+  })
